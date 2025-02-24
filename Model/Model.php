f32aa29370463123bb698abe4a594745939420c7
thomasr10 <t.rivoire01@gmail.com>
2025-02-24 14:50:52 +0100

    Class Model

---
+++ Model/Model.php
@@ -0,0 +1,22 @@
+<?php
+
+abstract class Model {
+
+    private static $db;
+
+    private static function setDb(){
+        try {
+            self::$db = new PDO("mysql:host=localhost;dbname=lutopia",'root', '');
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    private static function getDb(){
+        if(self::$db === null){
+            self::setDb();
+        }
+
+        return self::$db;
+    }
+}
