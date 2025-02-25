d28c1d383127714bfbf60aef3a8fd350f666333e
Nmec2 <contact.nmec@gmail.com>
2025-02-25 14:47:13 +0100

    Create Model book and display book img with title and editor

--- Controller/ControllerBook.php
+++ Controller/ControllerBook.php
@@ -2,6 +2,9 @@
 
 class ControllerBook {
     public function home(){
+        $model = new ModelBook();
+        $datas = $model->drawHome();
+        var_dump($datas);
         require_once('./View/homepage.php');
     }
 }
