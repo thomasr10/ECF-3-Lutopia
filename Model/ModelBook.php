5eda23f18016779170347fb9ed5e4ba8fdad8652
Nmec2 <contact.nmec@gmail.com>
2025-02-25 16:41:30 +0100

    add BookDTO with author and illustrator fullname and display in homepage

--- Model/ModelBook.php
+++ Model/ModelBook.php
@@ -2,12 +2,12 @@
 
 class ModelBook extends Model {
     public function drawHome(){
-        $req = $this->getDb()->query('SELECT `id_book`, `isbn`, `title`, `editor`, `img_src`, `publication_date`, `edition_date`, `synopsis`, `id_type`, `id_age` FROM `book`');
+        $req = $this->getDb()->query("SELECT `book`.`id_book`, `isbn`, `title`, `editor`, `img_src`, `publication_date`, `edition_date`, `synopsis`, `id_type`, `id_age`, CONCAT(`author`.`first_name`, ' ' ,`author`.`last_name`) AS 'author', CONCAT(`illustrator`.`first_name`, ' ',`illustrator`.`last_name`) AS 'illustrator' FROM `book` INNER JOIN `book_author` ON `book`.`id_book` = `book_author`.`id_book` INNER JOIN `author` ON `book_author`.`id_author` = `author`.`id_author` INNER JOIN `book_illustrator` ON `book`.`id_book` = `book_illustrator`.`id_book` INNER JOIN `illustrator` ON `book_illustrator`.`id_illustrator` = `illustrator`.`id_illustrator`;");
 
         $arrayobj = [];
 
         while($data = $req->fetch(PDO::FETCH_ASSOC)){
-            $arrayobj[] = new Book($data);
+            $arrayobj[] = new BookDTO(new Book($data), $data['author'], $data['illustrator']);
         }
         return $arrayobj;
     }
