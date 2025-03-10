7cb8ac3f4f6828e252cbb0b9cec89be1383bfa04
Mattheonpil <mpellet@net-c.fr>
2025-03-10 20:19:54 +0100

    amelioration menu burger

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,187 +1,163 @@
-const selectChild = document.getElementById('select-child');
-let booksContainer = document.getElementById('booksContainer');
-const childOption = document.querySelectorAll('.child')
+const selectChild = document.getElementById("select-child");
+let booksContainer = document.getElementById("booksContainer");
+const childOption = document.querySelectorAll(".child");
 // afficher les nouveautés en fonction de l'age de l'enfant séléctionné
 
 function displayNewBooks(age) {
-    let currentAge = age.value;
+  let currentAge = age.value;
 
-    fetch(`/home-category-age/${currentAge}`)
-    .then(response => response.json())
-    .then(data => {
-        let bookArticle = document.createElement('div');
-        bookArticle.className = 'images';
-        data.forEach(book => {
+  fetch(`/home-category-age/${currentAge}`)
+    .then((response) => response.json())
+    .then((data) => {
+      let bookArticle = document.createElement("div");
+      bookArticle.className = "images";
+      data.forEach((book) => {
+        let imgContainer = document.createElement("img");
+        imgContainer.className = "image";
+        imgContainer.setAttribute("src", book.img);
 
-            let imgContainer = document.createElement('img');
-            imgContainer.className = 'image';
-            imgContainer.setAttribute('src', book.img);
+        let authorContainer = document.createElement("span");
 
-            let authorContainer = document.createElement('span');
+        bookArticle.append(imgContainer);
+        bookArticle.append(authorContainer);
+        booksContainer = document.getElementById("bookContainer");
+        booksContainer.append(bookArticle);
 
-            bookArticle.append(imgContainer);
-            bookArticle.append(authorContainer);
-            booksContainer = document.getElementById('bookContainer');
-            booksContainer.append(bookArticle);
-
-            //à enlever c juste pour pas que l'image soit BALEZE
-            imgContainer.style.width = 200 + 'px'
-
-            
-         })
-    })
-}
-if(selectChild !== null){
-    
-    displayNewBooks(selectChild);
-    selectChild.addEventListener('change', function() {
-        sectionContainer.innerHTML = "";
-        displayNewBooks(selectChild);
+        //à enlever c juste pour pas que l'image soit BALEZE
+        imgContainer.style.width = 200 + "px";
+      });
     });
-
 }
-
+if (selectChild !== null) {
+  displayNewBooks(selectChild);
+  selectChild.addEventListener("change", function () {
+    sectionContainer.innerHTML = "";
+    displayNewBooks(selectChild);
+  });
+}
 
 let index = 0;
 
 function initCarousel() {
-    const imagesContainer = document.querySelector('.images');
-    imagesContainer.innerHTML = ''; // Réinitialise le conteneur avant d'ajouter de nouvelles images
+  const imagesContainer = document.querySelector(".images");
+  imagesContainer.innerHTML = ""; // Réinitialise le conteneur avant d'ajouter de nouvelles images
 }
 
 function showImage() {
-    const imagesContainer = document.querySelector('.images');
-    const totalImages = document.querySelectorAll('.image').length;
-    
-    // Calcul dynamique de la largeur pour adapter au nombre d'images
-    imagesContainer.style.transform = `translateX(${-index * 100 / totalImages}%)`; 
+  const imagesContainer = document.querySelector(".images");
+  const totalImages = document.querySelectorAll(".image").length;
+
+  // Calcul dynamique de la largeur pour adapter au nombre d'images
+  imagesContainer.style.transform = `translateX(${
+    (-index * 100) / totalImages
+  }%)`;
 }
 
 function next() {
-    const totalImages = document.querySelectorAll('.image').length;
-    
-    if (index < totalImages - 1) { 
-        index++;
-        showImage();
-    }
+  const totalImages = document.querySelectorAll(".image").length;
+
+  if (index < totalImages - 1) {
+    index++;
+    showImage();
+  }
 }
 
 function prev() {
-    if (index > 0) {
-        index--;
-        showImage();
-    }
+  if (index > 0) {
+    index--;
+    showImage();
+  }
 }
 
-
-document.getElementById('nextButton').addEventListener('click', next);
-document.getElementById('prevButton').addEventListener('click', prev);
-
-
-
-
-
-
-
-
-
-
-
+document.getElementById("nextButton").addEventListener("click", next);
+document.getElementById("prevButton").addEventListener("click", prev);
 
 // Debut menu burger-------------------------------------------------------
-const menuToggle = document.querySelector('.menu-toggle');
-const navbar = document.querySelector('.navbar');
-console.log(menuToggle);
-menuToggle.addEventListener('click', () => {
-    alert('ok')
-  navbar.classList.toggle('active');
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
+
+menuToggle.addEventListener("click", () => {
+  navbar.classList.toggle("active");
 });
 
 // Fin menu burger-------------------------------------------------------
 // afficher des propositions de livres
 
-
-
 const ageArray = [];
-childOption.forEach(age => {
-    ageArray.push(age.value);
+childOption.forEach((age) => {
+  ageArray.push(age.value);
 });
 
+const sectionContainer = document.getElementById("section-connected");
 
+function sendChildValue(array) {
+  console.log(array);
+  fetch("/home-section", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ ages: array }),
+  })
+    .then((response) => response.json())
+    .then((arrayObj) => {
+      let arrayId = [];
+      for (let i = 0; i < arrayObj.length; i++) {
+        arrayId.push(arrayObj[i].id_age);
+      }
+      uniqueId = Array.from(new Set(arrayId));
 
-const sectionContainer = document.getElementById('section-connected');
+      for (let i = 0; i < uniqueId.length; i++) {
+        let div = document.createElement("div");
+        div.setAttribute("id", `div-${i}`);
+        div.classList.add("row-container");
+        div.classList.add("booksContainer");
+        sectionContainer.append(div);
+      }
 
-function sendChildValue(array){
+      const rowContainers = document.querySelectorAll(".row-container");
+      const chunks = [];
+      const chunkSize = 4;
+      for (let i = 0; i < arrayObj.length; i += chunkSize) {
+        chunks.push(arrayObj.slice(i, i + chunkSize));
+      }
+      for (let i = 0; i < chunks.length; i++) {
+        for (let j = 0; j < chunks[i].length; j++) {
+          const bookArticle = document.createElement("article");
+          bookArticle.classList.add("card");
+          rowContainers[i].append(bookArticle);
+          const bookImg = document.createElement("img");
+          const bookTitle = document.createElement("p");
+          bookImg.setAttribute("src", chunks[i][j].img);
+          bookTitle.textContent = chunks[i][j].title;
+          const descLink = document.createElement("a"); //ajouter le lien
+          const borrowLink = document.createElement("a");
+          const linkDiv1 = document.createElement("div");
+          const linkDiv2 = document.createElement("div");
+          descLink.classList.add("button_pink");
+          borrowLink.classList.add("button_borrow");
+          linkDiv1.append(descLink);
+          linkDiv2.append(borrowLink);
+          descLink.setAttribute("href", "#");
+          borrowLink.setAttribute("href", "#");
+          descLink.textContent = "Voir la fiche";
+          borrowLink.textContent = "Réserver";
 
-    console.log(array)
-    fetch('/home-section', {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({ages: array}),
-    })
-    .then(response => response.json())
-    .then(arrayObj => {
-
-        let arrayId = [];
-        for(let i = 0; i < arrayObj.length; i++){
-             arrayId.push(arrayObj[i].id_age);
+          bookArticle.append(bookImg);
+          bookArticle.append(bookTitle);
+          bookArticle.append(linkDiv1);
+          bookArticle.append(linkDiv2);
         }
-        uniqueId = Array.from(new Set(arrayId));
-
-        for(let i = 0; i < uniqueId.length; i ++){
-            let div = document.createElement('div');
-            div.setAttribute('id', `div-${i}`);
-            div.classList.add('row-container');
-            div.classList.add('booksContainer');
-            sectionContainer.append(div);
-        }
-
-        const rowContainers = document.querySelectorAll('.row-container');
-        const chunks = [];
-        const chunkSize = 4;
-        for(let i = 0; i < arrayObj.length; i += chunkSize){
-            chunks.push(arrayObj.slice(i, i+ chunkSize));
-        }
-        for(let i = 0; i < chunks.length; i++){
-            for(let j = 0; j < chunks[i].length; j++){
-                const bookArticle = document.createElement('article');
-                bookArticle.classList.add('card');
-                rowContainers[i].append(bookArticle);
-                const bookImg = document.createElement('img');
-                const bookTitle = document.createElement('p');
-                bookImg.setAttribute('src', chunks[i][j].img)
-                bookTitle.textContent = chunks[i][j].title;
-                const descLink = document.createElement('a'); //ajouter le lien
-                const borrowLink = document.createElement('a');
-                const linkDiv1 = document.createElement('div');
-                const linkDiv2 = document.createElement('div');
-                descLink.classList.add("button_pink");
-                borrowLink.classList.add("button_borrow");
-                linkDiv1.append(descLink);
-                linkDiv2.append(borrowLink);
-                descLink.setAttribute('href', '#');
-                borrowLink.setAttribute('href', '#');
-                descLink.textContent = "Voir la fiche";
-                borrowLink.textContent = "Réserver";
-
-                bookArticle.append(bookImg);
-                bookArticle.append(bookTitle);
-                bookArticle.append(linkDiv1);
-                bookArticle.append(linkDiv2);
-
-            }
-            
-        }
-    })
-  
-    };
-
+      }
+    });
+}
 
 sendChildValue(ageArray);
 
-selectChild.addEventListener('change', function(event) {
-    const selectedAge = event.target.value;
-    const newArray = [selectedAge, ...ageArray.filter(age => age !== selectedAge)]
-    sectionContainer.innerHTML = "";
-    sendChildValue(newArray);
+selectChild.addEventListener("change", function (event) {
+  const selectedAge = event.target.value;
+  const newArray = [
+    selectedAge,
+    ...ageArray.filter((age) => age !== selectedAge),
+  ];
+  sectionContainer.innerHTML = "";
+  sendChildValue(newArray);
 });
