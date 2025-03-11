017cdb521aaa3931449c293baf2e0d3239cf102f
Chris181076 <mentachris@hotmail.fr>
2025-03-11 16:42:55 +0100

    cubesAnimation

---
+++ View/animation_2.php
@@ -0,0 +1,11 @@
+<div id="container_2">
+        <div class="images">
+        <img src="uploads/autres/age_2_3.webp" class="img6"alt="">
+        <img src="uploads/autres/age_2_4.webp" class="img7" alt="" >
+        <img src="uploads/autres/age_2_5.png" class="img8"alt="">
+        </div>
+        <div class="cubesGsap">
+        <img src="uploads/autres/age_2_1.webp" class="square img9">    
+        <img src="uploads/autres/age_2_2.webp" class="square img10">
+        </div>
+</div>
---
+++ View/animation_4.php
@@ -0,0 +1,13 @@
+<div id="container_4">
+<div class="cubesGsap">
+        <img src="uploads/autres/age_0_1.webp" class="square img4">
+        </div>
+        <div class="images">
+        <img src="uploads/autres/age_0_2.webp" class="img1"alt="">
+        <img src="uploads/autres/age_0_3.webp" class="img2" alt="" >
+        <img src="uploads/autres/age_0_4.webp" class="img3"alt="">
+        </div>
+        <div class="cubesGsap">      
+        <img src="uploads/autres/age_0_5.webp" class="square img5">
+        </div>
+</div>
