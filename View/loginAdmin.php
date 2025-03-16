7c723a4522c158583abafc666d46164b906edbd1
Chris181076 <mentachris@hotmail.fr>
2025-03-16 19:38:05 +0100

    loginAdmin

--- View/loginAdmin.php
+++ View/loginAdmin.php
@@ -1,5 +1,4 @@
 <?php
-
 $title = "Connexion Admin | Lutopia";
 $description = "Page de connexion admin à Lutopia";
 $pointSlash = "";
@@ -8,7 +7,9 @@ ob_start();
 <div>
    <?= (isset($message)) ? $message : '' ?> 
 </div>
+<div class="center">
 <form method="POST" action="/login-admin">
+    <div id="loginAdmin">
     <div>
         <label for="name">Name :</label>
         <input type="text" name="name" id="name">
@@ -22,9 +23,9 @@ ob_start();
         <input type="password" name="password" id="password">
     </div>
     <input type="submit" value="Connexion">
+    </div>
 </form>
-
-
+</div>
 <?php
 $content = ob_get_contents();
 ob_end_clean();
