a7a97316a188ede82c824cf8e0958865b8b986e6
Mattheonpil <mpellet@net-c.fr>
2025-03-17 16:50:09 +0100

    fin de l'accueil dashboard

--- View/dashboard.php
+++ View/dashboard.php
@@ -34,39 +34,45 @@ ob_start();
             echo 'Aucune carte sélectionnée';
         } else {
             ?> 
-            <hr>  <!-- hr a supprimer -->
-            <h2>Réservation en cours</h2>
-            <hr> <!-- hr a supprimer -->
+            <div class="dash-accueil-borrow">
+                
+            <h2>Réservation(s) en cours</h2>
+            
            <?php 
            foreach($reservation as $key=>$value):
            ?>
+           <div class="dash-accueil-reserv-contain">
            <p><?= $reservation[$key]->getTitle(); ?></p> <!-- titre de chaque livre réserver -->
            <?php 
            if($avaibility[$key][0]->getEnd_date()->format('Y-m-d') < $date){ // if bouton disponible ou else affichage indisponible
-                echo '<form method="POST" action="/dashboard?searchAdminUser=' . $_GET['searchAdminUser'] . '">                                
-                        <input type="submit" value="Disponible"> ' ;
+                echo '<form class= "dash-form-reserv" method="POST" action="/dashboard?searchAdminUser=' . $_GET['searchAdminUser'] . '">                                
+                        <button class = "dash-dispo" type="submit" value="Disponible">Disponible
+                          <img src="uploads/autres/keyboard_return.svg" alt="">
+                        </button> ' ;
             } else {
                 echo '
                 <form method="POST" action="/dashboard?searchAdminUser=' . $_GET['searchAdminUser'] . '"> 
-                <input class = "dash-disabled" type="submit" disabled value="Indisponible"> '; // information indisponibilité du livre
+                <button class = "dash-disabled" type="submit" disabled value="Indisponible">Indisponible  <img src="uploads/autres/keyboard_return.svg" alt=""></button> '; // information indisponibilité du livre
             }
             echo '</form>';
            ?> 
 
            <form method="POST" action="/dashboard<?= '?searchAdminUser=' . $_GET['searchAdminUser'] ?>">
             <input type="hidden" name="id_reservation" value="<?= $avaibility[$key][0]->getId_reservation();?>">
-            <input type="submit" name="cancel" value="Annuler">
+            <input class= "dash-input-annuler" type="submit" name="cancel" value="Annuler">
            </form>
+           </div>
            <?php endforeach;
            echo 3 - count($reservation). ' reservations restantes'; //nombre de réservation restante ?>
-           <hr> <!-- hr a supprimer -->
-
+            </div>
+            
            <div class="dash-accueil-borrow">
 
-            <h2>Emprunt(s) en cours</h2><hr>
+            <h2>Emprunt(s) en cours</h2>
 
             <?php
             foreach($search as $key=>$value): ?>
+            <div class="dash-accueil-borrow-contain">
                     <!-- <?=$search[$key]->getLast_Name() ?> -->
                     <!-- NOM DE L'USER POUR CHAQUE EMPRUNT  -->
 
@@ -79,8 +85,9 @@ ob_start();
                         <p class="dash-date-color">A rendre avant le : <span><?= $search[$key]->getEnd_date()->format('d-m-Y');?></span></p> <!-- date de fin de l'emprunt -->
                         <input type="hidden" name="id_borrow" value="<?= $search[$key]->getId_borrow()?>">
                     </div>
-                        <input type="date" name="date_back" value="<?= $search[$key]->getEnd_date()->format('Y-m-d');?>"> <!-- input date pour prolonger l'emprunt -->
-                        <input type="submit" name="prolong" value="Prolonger"> <!-- bouton qui prolonge l'emprunt -->
+                    <div class="dash-accueil-borrow-date">
+                        <input class="dash-date-choose" type="date" name="date_back" value="<?= $search[$key]->getEnd_date()->format('Y-m-d');?>"> <!-- input date pour prolonger l'emprunt -->
+                        <input class="dash-date-prolong"  type="submit" name="prolong" value="Prolonger"> <!-- bouton qui prolonge l'emprunt -->
                     </form>
 
                     <form method="POST" action="/dashboard<?= '?searchAdminUser=' . $_GET['searchAdminUser'] ?>"> <!-- formulaire pour rendre un livre emprunter -->
@@ -88,8 +95,9 @@ ob_start();
                         <input type="hidden" name="date_back" value="<?= $search[$key]->getEnd_date()->format('Y-m-d');?>">
                         <input type="submit" name="suppr" value="Rendre"> <!-- bouton rendre pour rendre un livre -->
                     </form>
-                    <hr>
-             
+                    
+                    </div>
+            </div> 
 
             <?php endforeach;
         } 
