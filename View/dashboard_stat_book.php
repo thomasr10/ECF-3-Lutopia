a749baac845613e837fdde7ad03a1926158d8d56
Mattheonpil <mpellet@net-c.fr>
2025-03-18 11:35:28 +0100

    creation base tableau

--- View/dashboard_stat_book.php
+++ View/dashboard_stat_book.php
@@ -5,7 +5,36 @@ ob_start();
 
 ?>
 
-<!-- TABLEAU STATISTIQUES LIVRES -->
+<section class="dash-statbook-globalcontainer">
+    <h2>Statistiques livres</h2>
+    <Article class="dash-statbook-container">
+        <table class="dash-statbook-table">
+            <thead class="dash-statbook-header">
+              <th scope="col" class = "statbook-title">Titre du livre</th>  
+              <th scope="col" class = "statbook-id">Id</th>  
+              <th scope="col" class = "statbook-author">Auteur</th>  
+              <th scope="col" class = "statbook-age">Age</th>  
+              <th scope="col" class = "statbook-type">Type</th>  
+              <th scope="col" class = "statbook-borrow">Emprunts</th>  
+            </thead>
+            <tbody class="dash-statbook-body">
+                <td class="dash-statbook-cellule">test1</td>
+                 <!-- cellules titre livre -->
+                <td class="dash-statbook-cellule">test2</td>
+                <!-- cellules Id -->
+                <td class="dash-statbook-cellule">test3</td>
+                <!-- cellules Auteurs -->
+                <td class="dash-statbook-cellule">test4</td>
+                <!-- cellules Ages -->
+                <td class="dash-statbook-cellule">test5</td>
+                <!-- cellules Types -->
+                <td class="dash-statbook-cellule">test5</td>
+                <!-- cellules emprunts -->
+            </tbody>
+
+        </table>
+    </Article>  
+</section>
 
 
 <?php
