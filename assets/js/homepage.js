9ea4b39d1a4b9bb488d591263d5ebac134cccc2b
Mattheonpil <mpellet@net-c.fr>
2025-03-19 11:17:43 +0100

    correction erreur bouton menu burger

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -126,23 +126,23 @@ function prev() {
 
 
 // Debut menu burger-------------------------------------------------------
-const menuToggle = document.querySelector('.menu-toggle');
-const navbar = document.querySelector('.navbar');
+// const menuToggle = document.querySelector('.menu-toggle');
+// const navbar = document.querySelector('.navbar');
 
-menuToggle.addEventListener("click", () => {
-    if (navbar.style.left === "0%") {
-      gsap.to(navbar, { left: "-80%", duration: 0.5, scale: 1 }); // Ferme le menu
+// menuToggle.addEventListener("click", () => {
+//     if (navbar.style.left === "0%") {
+//       gsap.to(navbar, { left: "-80%", duration: 0.5, scale: 1 }); // Ferme le menu
     
-    } else {
-      gsap.to(navbar, {
-        left: "0%",
-        duration: 0.5,
-        scale: 1, // Ouvre le menu avec une échelle de 1
-        ease: "back.out", // Effet de rebond
-      });
+//     } else {
+//       gsap.to(navbar, {
+//         left: "0%",
+//         duration: 0.5,
+//         scale: 1, // Ouvre le menu avec une échelle de 1
+//         ease: "back.out", // Effet de rebond
+//       });
     
-    }
-});
+//     }
+// });
 
 // Fin menu burger-------------------------------------------------------
 // afficher des propositions de livres
