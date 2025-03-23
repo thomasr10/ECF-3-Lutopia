d2b06a25c651b3eaa0545b51e17140e774ab8470
Mattheonpil <mpellet@net-c.fr>
2025-03-24 00:02:44 +0100

    modifier user ok

--- View/dashboard_update_user.php
+++ View/dashboard_update_user.php
@@ -12,7 +12,7 @@ ob_start();
 </div>
 
 
-<h1>Modifier un utilisateur</h1>
+<h1 class = "update-user-title">Modifier un utilisateur</h1>
 <div class = "dash-accueil-user">   
     <form class="dash-accueil-form" method="GET" action="/dashboard/update-user">
         <input type="text" name="user-card" id="searchAdminUser" value="" placeholder="Entrez ici le n° d'abonné">
@@ -24,47 +24,53 @@ ob_start();
 <?php
 if(!empty($user) && !empty($children)){
 ?>
+
+<div class="update-user-container">
 <form action="/dashboard/update-user" method="POST">
-    <div>
+    <div class="update-user-form1">
+    <div class="update-user-entry">
         <label for="first-name">Prénom</label>
         <input type="text" name="first-name" id="first-name" value="<?=$user->getFirst_name() ?>" required>
     </div>
-    <div>
+    <div class="update-user-entry">
         <label for="last-name">Nom de famille</label>
         <input type="text" name="last-name" id="last-name" value="<?=$user->getLast_name() ?>" required>
     </div>
-    <div>
+    <div class="update-user-entry">
         <label for="email">Adresse mail</label>
         <input type="text" name="email" id="email" value="<?=$user->getEmail() ?>" required>
     </div>
     <div>
         <input type="hidden" name="id_user" value="<?= $user->getId_user()?>">
     </div>
+    </div>
     <?php foreach($children as $child){ ?>
         <!-- div en dessous à flex pour aligner le prénom et la date comme sur figma -->
-        <div>
-            <div>
+        <div class=" update-user-border">
+            <div class="update-user-sub-entry">
                 <!-- bouton en dessous : trouver l'image du figma. Gardez bien la value et la class si vous changez le bouton -->
                 <button class="delete-child-btn" value="<?= $child->getId_child() ?>" type="button">Supprimer</button>
                 <label for="child-name">Prénom</label>
                 <input type="text" value="<?=$child->getName()?>" name="child-name[]" id="child-name">
                 <input type="hidden" name="id_child[]" id="id_child" value="<?=$child->getId_child()?>">
             </div>
-            <div>
+            <div class="update-user-sub-entry">
                 <label for="child-birth">Date de naissance</label>
                 <input type="date" name="child-birth[]" id="child-birth" value="<?=$child->getBirth_date()->format('Y-m-d')?>">
             </div>        
         </div>
     <?php } ?>
-    <div>
-        <div id="add-child"></div>
+    
+        <div class = "update-user-addchild">
+       
         <button id="add-child-btn" type="button">Ajouter un enfant</button>
-    </div>  
-    <div>
+        </div>  
+    <div class = "update-user-modifications">
         <input type="submit" value="Valider les modifications" name="update-user">
         <input type="reset" value="Annuler les modifications">
     </div>
 </form>
+</div>
 <?php
 }
 ?>
